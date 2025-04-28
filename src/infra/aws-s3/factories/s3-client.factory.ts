b40fd46e9f6dc71133c7ec50de8fec2a65fb87a1
jribas-dev <3b3brazil@gmail.com>
2025-04-28 17:35:16 -0300

    fix env variables

--- src/infra/aws-s3/factories/s3-client.factory.ts
+++ src/infra/aws-s3/factories/s3-client.factory.ts
@@ -7,8 +7,8 @@ export const s3ClientFactory = {
   provide: S3_CLIENT,
   useFactory: (configService: ConfigService): S3Client => {
     const region = configService.get<string>('AWS_REGION') as string;
-    const aKeyId = configService.get<string>('AWS_ACCESS_KEY_ID') as string;
-    const sKeyId = configService.get<string>('AWS_SECRET_ACCESS_KEY') as string;
+    const aKeyId = configService.get<string>('AWS_S3_KEY') as string;
+    const sKeyId = configService.get<string>('AWS_S3_SECRET') as string;
 
     return new S3Client({
       region: region,
--- src/infra/aws-ses/factories/ses-client.factory.ts
+++ src/infra/aws-ses/factories/ses-client.factory.ts
@@ -7,8 +7,8 @@ export const SesClientFactory = {
   provide: SES_CLIENT,
   useFactory: (configService: ConfigService): SESv2Client => {
     const region = configService.get<string>('AWS_REGION') as string;
-    const aKeyId = configService.get<string>('AWS_ACCESS_KEY_ID') as string;
-    const sKeyId = configService.get<string>('AWS_SECRET_ACCESS_KEY') as string;
+    const aKeyId = configService.get<string>('AWS_SES_KEY') as string;
+    const sKeyId = configService.get<string>('AWS_SES_PASS') as string;
 
     return new SESv2Client({
       region: region,
