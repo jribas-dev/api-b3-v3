8fa766e371161347cbea02bbfb4ba77e5dcd57b2
jribas-dev <3b3brazil@gmail.com>
2026-04-24 16:57:05 -0300

    tenant/cfg route - return cfg value from param

--- src/tenant/tenant.controller.ts
+++ src/tenant/tenant.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { UserInstanceGuard } from 'src/auth/guards/user-instance.guard';
+import { CfgService, CfgValue } from './cfg.service';
 import { EmpService } from './emp.service';
 
 @Controller('tenant')
 @UseGuards(JwtGuard, UserInstanceGuard)
 export class TenantController {
-  constructor(private readonly empService: EmpService) {}
+  constructor(
+    private readonly empService: EmpService,
+    private readonly cfgService: CfgService,
+  ) {}
 
   @Get('emitentes')
   async emitentes(@Request() req: { user: { userId: string; dbId: string } }) {
     return this.empService.listEmitentes(req.user.dbId, req.user.userId);
+  }
+
+  @Get('cfg')
+  async cfg(
+    @Request() req: { user: { dbId: string } },
+    @Query('param') param: string,
+  ): Promise<CfgValue> {
+    return this.cfgService.get(req.user.dbId, param);
   }
 }
